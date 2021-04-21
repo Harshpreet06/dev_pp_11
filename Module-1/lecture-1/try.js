cddba9601dfec11eccf15fd9c95ce83d0f813d57
Harshpreet <harshpreetsinghanand@gmail.com>
2021-04-21 17:20:22 +0530

    trying

---
+++ Module-1/lecture-1/try.js
@@ -0,0 +1,16 @@
+let a;
+console.log(a);
+a=10;
+console.log(a);
+if(true)
+{
+    a=20;
+    console.log(a);
+}
+console.log(a);
+if(true)
+{
+    let a=30;
+    console.log(a);
+}
+console.log(a);
